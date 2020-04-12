6682eed8629d2b7698eacea1550d7aea6a312401
Yudai Watanabe <lock0308@gmail.com>
2020-04-12 16:45:43 +0900

    FABタップで本検索ページへ遷移する機能追加。

---
+++ app/src/main/java/com/example/bookmanager/BookSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_search)
+    }
+}
--- app/src/main/java/com/example/bookmanager/MainActivity.kt
+++ app/src/main/java/com/example/bookmanager/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.bookmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +23,12 @@ class MainActivity : AppCompatActivity() {
 
         val decorator = DividerItemDecoration(this, manager.orientation)
         recyclerView?.addItemDecoration(decorator)
+
+        val fab: View = findViewById(R.id.fabAddBook)
+        fab.setOnClickListener {
+            val intent = Intent(applicationContext, BookSearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun createDummyData(): MutableList<MyBook> {
