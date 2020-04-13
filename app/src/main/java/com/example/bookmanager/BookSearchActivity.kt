e3228f79a6f57a34ae8ec9872b0aa821af629db8
Yudai Watanabe <lock0308@gmail.com>
2020-04-13 10:25:22 +0900

    戻るボタン押下による画面遷移処理を実装。

--- app/src/main/java/com/example/bookmanager/BookSearchActivity.kt
+++ app/src/main/java/com/example/bookmanager/BookSearchActivity.kt
@@ -1,9 +1,8 @@
 package com.example.bookmanager
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class BookSearchActivity : AppCompatActivity() {
@@ -15,10 +14,18 @@ class BookSearchActivity : AppCompatActivity() {
         setUpToolbar()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun setUpToolbar() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         toolbar.title = "search"
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
     }
 }
