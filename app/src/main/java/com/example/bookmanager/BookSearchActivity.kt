4db5addbdf61dcd9305e71ac8492f446ed2d1d28
Yudai Watanabe <lock0308@gmail.com>
2020-04-26 16:03:56 +0900

    検索結果が見つからなかった場合に発生するエラーを修正。

--- app/src/main/java/com/example/bookmanager/BookSearchActivity.kt
+++ app/src/main/java/com/example/bookmanager/BookSearchActivity.kt
@@ -95,6 +95,8 @@ class BookSearchActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.book_search_results)
         val adapter = recyclerView.adapter as BookSearchResultsAdapter
         val cnt = adapter.itemCount
+        if (cnt < 1) return
+
         adapter.removeAll()
         for (i in (cnt - 1)..0) {
             adapter.notifyItemRemoved(i)
