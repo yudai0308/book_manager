5d246ba650d12c51301a8ce2defa37413af2f418
Yudai Watanabe <lock0308@gmail.com>
2020-04-29 22:50:58 +0900

    不要なファイルを削除。

--- app/src/main/java/com/example/bookmanager/MyBook.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bookmanager
-
-class MyBook(image: Int, title: String, authors: List<String>) {
-    var mImage: Int = image
-    var mTitle: String = title
-    var mAuthors: List<String> = authors
-}
--- app/src/main/java/com/example/bookmanager/NewBook.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bookmanager
-
-class NewBook(title: String, authors: List<String>, image: String) {
-    var mTitle: String = title
-    var mAuthors: List<String> = authors
-    var mImage: String = image
-}
